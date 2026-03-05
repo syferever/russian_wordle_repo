98eec869b58db572a5a7f6a3a78d3db1f6d9e5b0
Kirill Karasev <kir.karasyov2017@yandex.ru>
2026-03-05 16:06:19 +0300

    removed bin/

--- bin/russian_wordle_repo.dart
+++
@@ -1,5 +0,0 @@
-import 'package:russian_wordle_repo/russian_wordle_repo.dart' as russian_wordle_repo;
-
-void main(List<String> arguments) {
-  print('Hello world: ${russian_wordle_repo.calculate()}!');
-}
